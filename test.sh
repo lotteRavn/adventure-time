013a85013731acb7b29cbde8d30e64658a35ca0e
Lotte <lotteravn@live.dk>
2016-08-25 13:53:21 +0200

    test adapted

--- test.sh
+++ test.sh
@@ -17,7 +17,7 @@ function testString {
     fi
 }
 
-testString "Finn should say ADVENTURE TIME" ./finn.sh 'ADVENTURE TIME'
+testString "Finn should say ADVENTURE TIME" ./finn.sh 'Christmas TIME'
 testString "Jake should say Mathemagical" ./jake.sh 'Mathemagical'
 
 echo "$testsPassed unit tests passed"
